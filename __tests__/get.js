7ade7343006aeac2ee268aba66a87b178d899481
sagostinelli <batonenbois@gmail.com>
2020-06-10 18:38:29 -0400

    extended get optiobns

--- __tests__/get.js
+++ __tests__/get.js
@@ -5,10 +5,17 @@ it('gets a file', async () => {
   const file = await get({
     apiKey: '123',
     apiSecret: 'abc',
-    key: 'my-file'
+    key: 'my-file',
+    getOptions: ['data', 'key', 'hash', 'publicUrl']
   });
 
-  const expectedResult = {"data": "file-content"};
+  const expectedResult = 
+  {
+    data: 'file-content',
+    key: 'my-file',
+    hash: '123',
+    publicUrl: 'https://bucket-1.storage.fleek.co/my-file'
+  };
 
   expect(file).toEqual(expectedResult);
 });
