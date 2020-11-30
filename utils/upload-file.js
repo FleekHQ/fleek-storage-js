6cac07f726926a50de6173f7809aaed131c50d57
studna <vojtech.studenka@gmail.com>
2020-11-30 15:18:18 +0100

    Update upload-file.js

--- utils/upload-file.js
+++ utils/upload-file.js
@@ -2,8 +2,8 @@ const uploadFile = (s3, params) => new Promise((resolve, reject) => {
   const request = s3.putObject(params);
   request
     .on('success', (response) => {
-      const hash = response.httpResponse.headers['x-fleek-ipfs-hash'];
-      const hashV0 = response.httpResponse.headers['x-fleek-ipfs-hash-v0'];
+      const hash = response.httpResponse.headers['x-fleek-ipfs-hash'] || response.httpResponse.headers['X-Fleek-Ipfs-Hash'];
+      const hashV0 = response.httpResponse.headers['x-fleek-ipfs-hash-v0'] || response.httpResponse.headers['X-Fleek-Ipfs-Hash-V0'];
 
       resolve ({
         hash,
