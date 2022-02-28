c16c7efbe6fb85d8cc08c046fbcb4e834c146495
Lu Zhu <misterzhu@gmail.com>
2022-02-28 10:31:58 -0800

    Add promise rejection on error to upload-file (#37)

--- utils/upload-file.js
+++ utils/upload-file.js
@@ -5,24 +5,28 @@ const uploadFile = (s3, params, cb = () => {}) => (new Promise((resolve, reject)
   const request = s3.putObject(params);
 
   request.on('complete', (response) => {
-    const { data: { ETag } } = response;
-
-    const hash = ETag.replace(/^"|"$/g, '');
-
-    const cidObj = CID.parse(hash);
-  
-    let cidv0;
-  
-    const cidv1 = cidObj.toV1().toString();
-
     try {
-      cidv0 = cidObj.toV0().toString();
-    } catch (e) {
-      // fallback when cbor is used
-      cidv0 = cidv1;
-    }
+      const { data: { ETag } } = response;
 
-    resolve ({ hash: cidv1, hashV0: cidv0 });
+      const hash = ETag.replace(/^"|"$/g, '');
+
+      const cidObj = CID.parse(hash);
+
+      let cidv0;
+
+      const cidv1 = cidObj.toV1().toString();
+
+      try {
+        cidv0 = cidObj.toV0().toString();
+      } catch (e) {
+        // fallback when cbor is used
+        cidv0 = cidv1;
+      }
+
+      resolve ({ hash: cidv1, hashV0: cidv0 });
+    } catch (e) {
+      reject(e);
+    }
   });
 
   request.on('error', (error) => {
