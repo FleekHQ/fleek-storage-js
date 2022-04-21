62d7ab51ccd730e5cf0ded73cc74d183dab71894
Brett Henderson <bretth18@gmail.com>
2022-04-21 15:01:20 -0700

    modify endpoint config for storageapi2.fleek.co

--- utils/get-public-url.js
+++ utils/get-public-url.js
@@ -1,5 +1,5 @@
 const getPublicUrl = (bucket, filename) => (
-  `https://storageapi.fleek.co/${bucket}/${filename}`
+  `https://storageapi2.fleek.co/${bucket}/${filename}`
 );
 
 module.exports = getPublicUrl;
