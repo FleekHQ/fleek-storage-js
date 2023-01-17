c25dc3dc0d4f27729434c9fd107a88e2adefb026
germi <germanbonin@gmail.com>
2023-01-17 08:50:57 -0700

    Replace config values with dev env

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const config = {
   ipfsGateway: 'https://ipfs.fleek.co/ipfs',
-  storageEndpoint: 'https://storageapi.fleek.one',
-  fleekGraphQl: 'https://h6qbvxquqjg5direndhm7ugaj4.appsync-api.us-west-2.amazonaws.com/graphql'
+  // storageEndpoint: 'https://storageapi.fleek.one',
+  // fleekGraphQl: 'https://h6qbvxquqjg5direndhm7ugaj4.appsync-api.us-west-2.amazonaws.com/graphql',
+  storageEndpoint: 'https://storageapi-dev.fleek.host',
+  fleekGraphQl: 'https://7w65cjs2fnbwzdsltmxj427wfu.appsync-api.us-west-2.amazonaws.com/graphql'
 };
 
 module.exports = config;
